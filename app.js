183623d500e1e02437eef271d6b19d3dc2d66ff5
Adam Brown <downtownadambrown@gmail.com>
2018-10-08 12:15:00 -0400

    Added fancy pop-up to add new company button

--- app.js
+++ app.js
@@ -1,52 +1,79 @@
-$(document).ready(function(){
-    
-    const clearButtons = function() {
-        $('.button').remove();
-    };
 
-    const request = function(){
-        ticker = $(this).text();
+const clearButtons = function () {
+    $('button').remove();
+};
 
-        //Added security to the queryURL
-        const queryURL = `https://api.iextrading.com/1.0/stock/${encodeURIComponent(ticker)}/batch?types=company,quote,logo,news`; 
+const request = function () {
+    ticker = $(this).text();
 
-        $.ajax({
-            url: queryURL,
-            type: 'GET'
-        }).then(function(response){
-            console.log(response);
-            const companyName = response.company.companyName;
-            const companyLogo = response.logo.url;
-            const companyPrice = response.quote.latestPrice;
-            const companyNews = response.news;
-            const companyTicker = response.company.symbol
-            
-            const companyHTML = `<img class="companyLogo m-3" src="${companyLogo}"><h2>(${companyTicker})${companyName}</h2>`;
-            
-            $('.companyInfo').html(companyHTML);
-           
+    //Added security to the queryURL
+    const queryURL = `https://api.iextrading.com/1.0/stock/${encodeURIComponent(ticker)}/batch?types=company,quote,logo,news`;
 
-            let newslistHTML = ``;
-            
-            for (let i = 0; i < companyNews.length; i++){
-                newslistHTML += `<div class="news" id="news${i}></div>`;
-            }
-            //finalize newslistHTML output to main col
+    $.ajax({
+        url: queryURL,
+        type: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        const companyName = response.company.companyName;
+        const companyLogo = response.logo.url;
+        const companyPrice = response.quote.latestPrice;
+        const companyNews = response.news;
+        const companyTicker = response.company.symbol
+        const companySummaryText = response.company.description;
 
-        });
-    };
+        //Set the company info header
+        const companyHTML = $(`<img class="companyLogo p-3" src="${companyLogo}"><h2>(${companyTicker})${companyName}</h2>`);
+        const companySummary = `<p id="companySummary">${companySummaryText}</p>`;
+        $('.companyInfo').html(companyHTML);
+        $('.companyInfo').append($(companySummary))
 
-    const renderButtons = function() {
-        clearButtons();
+        //Set the company summary
 
-        for (let i = 0; i < stockList.length; i++){
-            const buttonHTML = $(`<button type='button' class='btn btn-dark m-2' id='button${i}'>${stockList[i].toUpperCase()}</button>`);
-            buttonHTML.on(`click`, request);
-            $(`.buttonList`).append(buttonHTML);
+        //console.log(companySummary);
+        //$('.companySummary').html(companySummary);
+
+        //Set the company news divs
+        var currentNews = ``;
+        for (let i = 0; i < companyNews.length; i++) {
+            currentNews += `<div class="news bg-secondary p-3 border-bottom" id="news${i}"></div>`;
         }
-    };
-    
-    //global variables
-    const stockList = ['GOOG','FB','AMZN','SHAK','SNAP', 'SWK'];
-    renderButtons();   
-});
+        //finalize newslistHTML output to main col           
+        $(`.companyNews`).html($(currentNews));
+
+    });
+};
+
+const renderButtons = function () {
+    clearButtons();
+
+    for (let i = 0; i < stockList.length; i++) {
+        const buttonHTML = $(`<button type='button' class='btn btn-dark m-2' id='button${i}'>${stockList[i].toUpperCase()}</button>`);
+        buttonHTML.on(`click`, request);
+        $(`.buttonList`).append(buttonHTML);
+    }
+};
+
+const addCompany = function () {
+    bootpopup({
+        showclose: false,
+        buttons: ["cancel", "ok"],
+        title: "Create a new button",
+        content: [
+            `<p class="lead">Enter a new stock ticker symbol</p>`,
+            { input: { type: "text", label: "", name: "title", placeholder: "Ex: Google is GOOG"} }],
+        ok: function (data, e) {
+            let companyInput = data.title.toUpperCase();
+            stockList.push(companyInput);
+            renderButtons();
+        },
+        complete: function () { },
+    });
+};
+
+//global variables
+const stockList = ['GOOG', 'FB', 'AMZN', 'SHAK', 'SNAP', 'SWK'];
+
+//events
+$('img#addIcon').on('click', addCompany);
+renderButtons();
+
